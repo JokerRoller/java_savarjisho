1a217a972aa592c22416af9076fb3499c5830cea
JokerRoller <BECOOL686868@gmail.com>
2022-06-03 15:01:02 +0400

    asd

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
-    }
+function Car ( make, model, year ) {
+    this.make = make,
+    this.model = model,
+    this.year = year
+    this.getCarInfo = function(){
+        document.write( this.make + ' ' + this.model + ' released in ' + this.year + "</br>" )
+    };
 };
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function() {
-        console.log('draw');
-    }
+function Person ( name, age, cars ) {
+    this.name = name,
+    this.age = age,
+    this.cars = cars
+    this.getPersonInfo = function() {
+        document.write( this.name + ' არის' + this.age + ' წლის და ყავს ' + this.cars + ' ' ) + car.getCarInfo()
+    };
 };
+
+var car = new Car('Tesla', 'Model S', 2015);
+car.getCarInfo();
+
+var person = new Person( 'ტომი', 24, 'მანქანა' );
+person.getPersonInfo(); 
